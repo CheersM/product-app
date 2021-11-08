f06f9675844dac152a2a4815f2168c444fa61e19
WIN-6OHIEJTVVGG\User <vdiukarev90@gmail.com>
2021-11-08 20:23:20 +0200

    fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react';
 
 const Footer: FC = () => {
-  return <footer>2021 © COPYRIGHT</footer>;
+  return (
+    <footer>
+      <span className="footer">2021 © COPYRIGHT</span>
+    </footer>
+  );
 };
 
 export default Footer;
